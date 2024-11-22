b6c0f75aa34171dcd87bf9c5f66b4c9910877fd4
Venilson Neves <103954028+venilson0708@users.noreply.github.com>
2024-11-22 08:11:50 -0300

    Começando com recursividade

---
+++ recursividade/somaNaturais.js
@@ -0,0 +1,12 @@
+function somaNaturais(n){
+    if(n === 0){
+        return 0;
+    }
+    else{
+        return n + somaNaturais(n - 1);
+    }
+}
+
+const teste = somaNaturais(50);
+
+console.log(teste);
