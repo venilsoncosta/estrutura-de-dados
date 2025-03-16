9d8b1fdaa75c43cd2b3ba1fe006604cd62844eba
Venilson Neves <venilsoncosta21@gmail.com>
2025-03-16 09:58:58 -0300

    criado o bubble sort

--- ordenacao/bubble_sort.js
+++ ordenacao/bubble_sort.js
@@ -1,13 +1,16 @@
 const bubbleSort = (nums) => {
     const N = nums.length;
     for(let i = 0; i < N; i++) {
-        for(let j = 0; j < N - 1; j++) {
+        let swapped = false;
+        for(let j = 0; j < N - 1 -i; j++) {
             if(nums[j] > nums[j + 1]){
                 const aux = nums[j];
                 nums[j] = nums[j + 1];
                 nums[j + 1] = aux;
+                swapped = true;
             }
         }
+        if(!swapped) break;
     }
     return nums;
 }
